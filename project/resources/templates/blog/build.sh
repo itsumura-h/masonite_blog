24cb97ee04fb4e99d7511308982859e4b41f3a77
itsumura-h <dumblepy@gmail.com>
2019-02-11 22:38:34 +0900

    sw完成

--- project/resources/templates/blog/build.sh
+++ project/resources/templates/blog/build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# service-worker.jsの「index.html」を「blog」に置換
 sed -i -e "s/index.html/blog/" build/service-worker.js
+
+# service-worker.jsの「precache-manifest」を「static/precache-manifest」に置換
 sed -i -e "s?precache-manifest?static/precache-manifest?" build/service-worker.js
+# precache-manifestのファイルをstatic以下に移動
 mv build/precache-manifest.* build/static/
