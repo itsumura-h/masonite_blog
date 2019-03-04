c2645f175e53bb40cf6b871c57cd8b6e7f8a5a81
itsumura-h <dumblepy@gmail.com>
2019-03-04 12:58:13 +0900

    URLは名詞のみ、メソッドは動詞付きに変更

--- project/app/http/controllers/BlogController.py
+++ project/app/http/controllers/BlogController.py
@@ -22,55 +22,55 @@ class BlogController:
 
 
     #_/_/_/_/_/_/_/_/  API  /_/_/_/_/_/_/_/_/
-    def toppage(self, request:Request):
+    def get_toppage(self, request:Request):
         toppage = Toppage.toppage().serialize()
         return {'value': {'toppage': toppage}}
 
-    def toppage_en(self, request:Request):
+    def get_toppage_en(self, request:Request):
         toppage = Toppage.toppage_en()
         return {'value': {'toppage': toppage}}
     #----------------------------------------------------
-    def series(self, request:Request):
+    def get_series(self, request:Request):
         series = Series.series()
         return{'value': {'series': series}}
 
-    def series_en(self, request:Request):
+    def get_series_en(self, request:Request):
         series = Series.series_en()
         return{'value': {'series': series}}
     #----------------------------------------------------
-    def articles(self, request:Request):
+    def get_articles(self, request:Request):
         series_id = request.param('series_id')
         series = Series.series_mono(series_id)
         articles = Article.articles_by_series_id(series_id)
         return {'value':{'series':series, 'articles': articles}}
 
-    def articles_en(self, request:Request):
+    def get_articles_en(self, request:Request):
         series_id = request.param('series_id')
         series = Series.series_mono_en(series_id)
         articles = Article.articles_by_series_id_en(series_id)
         return {'value':{'series':series, 'articles': articles}}
     #-----------------------------------------------------
-    def article(self, request:Request):
+    def get_article(self, request:Request):
         timestamp = request.param('timestamp')
         article = Article.article_by_timestamp(timestamp)
         article['tags'] = Tag.tags_by_timestamp(timestamp)
         return {'value': {'article': article}}
 
-    def article_en(self, request:Request):
+    def get_article_en(self, request:Request):
         timestamp = request.param('timestamp')
         article = Article.article_by_timestamp_en(timestamp)
         article['tags'] = Tag.tags_by_timestamp_en(timestamp)
         return {'value': {'article': article}}
     #-----------------------------------------------------
-    def diaries(self, request:Request):
+    def get_diaries(self, request:Request):
         diaries = Article.diaries()
         return{'value': {'diaries': diaries}}
 
-    def diaries_en(self, request:Request):
+    def get_diaries_en(self, request:Request):
         diaries = Article.diaries_en()
         return{'value': {'diaries': diaries}}
     #-----------------------------------------------------
-    def articles_by_keyword(self, request:Request):
+    def get_articles_by_keyword(self, request:Request):
         keyword = request.input('keyword')
         if keyword == '':
             return {'value': ''}
@@ -78,7 +78,7 @@ class BlogController:
         articles = Article.articles_by_keyword(keyword)
         return {'value': {'keyword': keyword, 'articles': articles}}
 
-    def articles_by_keyword_en(self, request:Request):
+    def get_articles_by_keyword_en(self, request:Request):
         keyword = request.input('keyword')
         if keyword == '':
             return {'value': ''}
@@ -86,13 +86,13 @@ class BlogController:
         articles = Article.articles_by_keyword_en(keyword)
         return {'value': {'keyword': keyword, 'articles': articles}}
     #-----------------------------------------------------
-    def articles_by_tag_id(self, request:Request):
+    def get_articles_by_tag_id(self, request:Request):
         tag_id = request.param('tag_id')
         tag = Tag.tag_name_by_tag_id(tag_id)
         articles = Article.articles_by_tag_id(tag_id)
         return {'value': {'tag': tag, 'articles': articles}}
 
-    def articles_by_tag_id_en(self, request:Request):
+    def get_articles_by_tag_id_en(self, request:Request):
         tag_id = request.param('tag_id')
         tag = Tag.tag_name_by_tag_id_en(tag_id)
         articles = Article.articles_by_tag_id_en(tag_id)
--- project/routes/web.py
+++ project/routes/web.py
@@ -14,26 +14,26 @@ ROUTES = [
 
     RouteGroup([
         RouteGroup([
-            get('/toppage', BlogController.toppage),
-            get('/toppage_en', BlogController.toppage_en),
+            get('/toppage', BlogController.get_toppage),
+            get('/toppage_en', BlogController.get_toppage_en),
 
-            get('/series', BlogController.series),
-            get('/series_en', BlogController.series_en),
+            get('/series', BlogController.get_series),
+            get('/series_en', BlogController.get_series_en),
 
-            get('/articles/@series_id:int', BlogController.articles),
-            get('/articles_en/@series_id:int', BlogController.articles_en),
+            get('/articles/@series_id:int', BlogController.get_articles),
+            get('/articles_en/@series_id:int', BlogController.get_articles_en),
 
-            get('/article/@timestamp:int', BlogController.article),
-            get('/article_en/@timestamp:int', BlogController.article_en),
+            get('/article/@timestamp:int', BlogController.get_article),
+            get('/article_en/@timestamp:int', BlogController.get_article_en),
 
-            get('/diaries', BlogController.diaries),
-            get('/diaries_en', BlogController.diaries_en),
+            get('/diaries', BlogController.get_diaries),
+            get('/diaries_en', BlogController.get_diaries_en),
 
-            get('/articlesByKeyword', BlogController.articles_by_keyword),
-            get('/articlesByKeyword_en', BlogController.articles_by_keyword_en),
+            get('/articlesByKeyword', BlogController.get_articles_by_keyword),
+            get('/articlesByKeyword_en', BlogController.get_articles_by_keyword_en),
 
-            get('/articlesByTagId/@tag_id:int', BlogController.articles_by_tag_id),
-            get('/articlesByTagId_en/@tag_id:int', BlogController.articles_by_tag_id_en),
+            get('/articlesByTagId/@tag_id:int', BlogController.get_articles_by_tag_id),
+            get('/articlesByTagId_en/@tag_id:int', BlogController.get_articles_by_tag_id_en),
         ], prefix='/api'),
 
         get('', BlogController.show),
