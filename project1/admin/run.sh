5129246cab26f1e8408d24e2d89b8ad5f35508f0
itsumura-h <dumblepy@gmail.com>
2019-06-17 23:48:22 +0900

    fix admin run.sh

--- project1/admin/run.sh
+++ project1/admin/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # source ../../.vpy7/bin/activate
-source ../.py36711/bin/activate
+source ../../.py36711/bin/activate
 nohup gunicorn admin.wsgi --config gunicorn_django.py > /dev/null 2>&1 &
