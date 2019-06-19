ca326ee04939f1e5269b3c076acac689796dcdf0
itsumura-h <dumblepy@gmail.com>
2019-06-20 00:14:00 +0900

    css遅延読み込み build

--- project/resources/templates/blog/build.sh
+++ project/resources/templates/blog/build.sh
@@ -1,5 +1,2 @@
 # main.********.chunk.jsを遅延読み込み
 sed -i -e s?'<script src="/static/js/main'?'<script async src="/static/js/main'? build/index.html
-
-# main.********.chunk.cssを遅延読み込み
-sed -i -e s?'<link href="/static/css/main'?'<link rel="preload" href="/static/css/main'? build/index.html
